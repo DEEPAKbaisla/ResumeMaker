2369df4576e44258d9ba9788d74f36b93246a4cd
Deepak <dkbainsla8@gmail.com>
2025-11-05 11:43:11 +0530

    cors update

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // app.use(cors());
 app.use(
   cors({
-    origin: "http://localhost:5173"||  "https://resumebuilder-silk-theta.vercel.app", // your Vite frontend URL
+    origin: "https://resumebuilder-silk-theta.vercel.app", // your Vite frontend URL
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"], // ✅ crucial
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
